958d8ca5c1b2c8167b423b31a168bf70eefe4ef0
young <youngyouwu@163.com>
2017-10-07 21:21:32 +0800

    修改employee.java文件，添加一个属性

--- src/main/java/io/young/springboot/exception/EmployeeException.java
+++ src/main/java/io/young/springboot/exception/EmployeeException.java
@@ -1,4 +1,13 @@
 package io.young.springboot.exception;
 
 public class EmployeeException extends RuntimeException {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
